2c3152c17f45ff99acec9bf5d9ae45fa28078484
usmanashrf <45431758+usmanashrf@users.noreply.github.com>
2019-03-07 20:49:37 +0500

    Update C++ utility.cpp

--- C++ utility.cpp
+++ C++ utility.cpp
@@ -11,41 +11,46 @@ in.open("mustakbil's data.txt");
 ofstream out;
 out.open("mustakbilFinal.txt");
 
-string a,b,c,d,e,f,g,h,i,j,k,l;
-        int y=1;	
+string title,org,jobType;
+string	type,jobShift,shift;
+string	experience,years,sal;
+string	salary,decs,loc_date; 
+int Counter=1;	               // counter for count the total numbers of lines in files
+char delimiter="]";           // keep a unique delimeter
 	   {
 	 while (! in.eof())
 	       {
-                out<<y<<"  ";
-			    getline(in,a); 
-                out<<a;  
-                out<<"]";
-			    getline(in,b); 
-			    out<<b;
-			    out<<"]";
-				getline(in,c);
-				getline(in,d);
-				out<<d;
-				out<<"]";
-				getline(in,e);
-				getline(in,f);
-				out<<f;
-				out<<"]";
-				getline(in,g);
-				getline(in,h);
-				out<<h;
-				out<<"]";
-				getline(in,i);
-				getline(in,j);
-				out<<j;
-				out<<"]";
-				getline(in,k);
-				out<<k;
-				out<<"]";
-				getline(in,l);
-				out<<l;
-				out<<"]";
-				out<<endl;
+                out<<Counter<<"  ";
+		      // get each individual line in file
+			    getline(in,title); 
+                            out<<title;  
+                            out<<delimiter;
+			    getline(in,org); 
+			    out<<org;
+			    out<<delimiter;
+			    getline(in,jobType);
+			    getline(in,type);
+		            out<<type;
+                            out<<delimiter;
+			    getline(in,jobShift);
+			    getline(in,shift);
+			    out<<shift;
+			    out<<delimiter;
+			    getline(in,experience);
+			    getline(in,years);
+			    out<<years;
+			    out<<delimiter;
+			    getline(in,sal);
+			    getline(in,salary);
+			    out<<salary;
+			    out<<delimiter;
+			    getline(in,desc);
+			    out<<desc;
+			    out<<delimiter;
+			    getline(in,loc_date);
+			    out<<loc_date;
+			    out<<delimiter;
+			    out<<endl;
                 /*cout<<y<<endl;
                 cout<<a<<endl<<b<<endl<<d<<endl<<f<<endl<<h<<endl<<j<<endl<<k<<endl<<l;
                 cout<<endl;*/
